d748517cfc2499f7ec969367f2235a902abe32e4
Zhang Xin <z5125550@ad.unsw.edu.au>
2018-07-29 20:21:20 +1000

    all done

--- lab01-Github-lab-release/strings.py
+++ lab01-Github-lab-release/strings.py
@@ -1,2 +1,7 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
+for i in range(len(strings)):
+    sentence += strings[i] +' '
+sentence.strip()
+print(sentence)
+print(' '.join(strings))
